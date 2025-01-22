ea4886b9b2ec79f5ab49e5b2d2dd5bd10324add0
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-01-21 22:38:03 -0500

    Add support to set custom values

--- components/system_status/system_status.cpp
+++ components/system_status/system_status.cpp
@@ -12,6 +12,33 @@ namespace system_status {
 
 static const char *const TAG = "system_status";
 
+void SystemStatusItem::value_inc(int32_t value) {
+  this->is_string_ = false;
+  this->value_int_ += value;
+}
+
+void SystemStatusItem::value_dec(int32_t value) {
+  this->is_string_ = false;
+  this->value_int_ -= value;
+}
+
+void SystemStatusItem::value_set(int32_t value) {
+  this->is_string_ = false;
+  this->value_int_ = value;
+}
+
+void SystemStatusItem::value_set(std::string value) {
+  this->is_string_ = true;
+  this->value_str_ = value;
+}
+
+const std::string& SystemStatusItem::to_string() {
+  if (!this->is_string_) {
+    this->value_str_ = str_sprintf("%" PRIi32, this->value_int_);
+  }
+  return this->value_str_;
+}
+
 std::string SystemStatusComponent::get_uptime_() {
 #ifdef USE_ESP32
   uint32_t seconds = esp_timer_get_time() / 1000000;
@@ -29,13 +56,18 @@ std::string SystemStatusComponent::get_uptime_() {
 }
 
 void SystemStatusComponent::dump_config() {
-  ESP_LOGCONFIG(TAG, "System Status:");
-  ESP_LOGCONFIG(TAG, "  Frequency: %" PRIu32 " hz", arch_get_cpu_freq_hz());
-  ESP_LOGCONFIG(TAG, "  Uptime: %s", this->get_uptime_().c_str());
+  // update values and print
+  this->data_["Uptime"].value_set(this->get_uptime_());
+  this->data_["Frequency"].value_set(arch_get_cpu_freq_hz());
   this->dump_config_trigger_->trigger();
+  ESP_LOGCONFIG(TAG, "System Status:");
+  for (auto& i : this->data_) {
+    const std::string& value = i.second.to_string();
+    if (value != "") {
+      ESP_LOGCONFIG(TAG, "  %s: %s", i.first.c_str(), value.c_str());
+    }
+  }
 }
-
-void SystemStatusComponent::setup() { }
 
 }  // namespace system_status
 }  // namespace esphome
--- components/system_status/system_status.h
+++ components/system_status/system_status.h
@@ -1,20 +1,39 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include <map>
 
 namespace esphome {
 namespace system_status {
 
+class SystemStatusItem {
+ public:
+  void value_inc(int32_t value);
+  void value_dec(int32_t value);
+  void value_set(int32_t value);
+  void value_set(std::string value);
+  const std::string& to_string();
+ protected:
+  std::string value_str_;
+  int32_t value_int_{0};
+  bool is_string_{false};
+};
+
+
 class SystemStatusComponent : public Component {
  public:
   float get_setup_priority() const override { return setup_priority::LATE; }
-  void setup() override;
   void dump_config() override;
+  void value_inc(std::string id, int32_t value) { this->data_[id].value_inc(value); }
+  void value_dec(std::string id, int32_t value) { this->data_[id].value_dec(value); }
+  void value_set(std::string id, int32_t value) { this->data_[id].value_set(value); }
+  void value_set(std::string id, std::string value) { this->data_[id].value_set(value); }
   Trigger<> *get_dump_config_trigger() const { return this->dump_config_trigger_; };
 
  protected:
-  Trigger<> *dump_config_trigger_{new Trigger<>()};
   std::string get_uptime_();
+  std::map<std::string, SystemStatusItem> data_;
+  Trigger<> *dump_config_trigger_{new Trigger<>()};
 };
 
 }  // namespace system_status
